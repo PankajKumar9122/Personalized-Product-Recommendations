a0030bcd921d7fcd8ff61a347754a869588f86db
Pankaj Kumar <103299023+PankajKumar9122@users.noreply.github.com>
2023-08-19 08:37:29 +0530

    Delete ProductRecommendationSystem directory

--- ProductRecommendationSystem/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $host = "localhost";
-    $uname = "root";
-    $pwd = '';
-    $database = "product_recommendation_system";
-    $con = mysqli_connect($host, $uname, $pwd, $database) ;
-?>
